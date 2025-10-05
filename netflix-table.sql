75587f1e3c2cf30591134243711838a1538393a3
Prateek verma <alphamadcity9899@gmail.com>
2025-10-05 20:47:06 +0530

    Add files via upload

---
+++ netflix-table.sql
@@ -0,0 +1,19 @@
+--NETFLIX PROJECT
+drop table if exists netflix;
+create table netflix
+(show_id varchar(8),
+ type varchar(10),
+ title varchar(150),
+ director varchar(208),
+ casts varchar(1000),
+ country varchar(150),
+ date_added	varchar(50),
+ release_year int,
+ rating	varchar(10),
+ duration varchar(15),
+ listed_in varchar(80),
+ description varchar(250)
+);
+
+select * from netflix
+
